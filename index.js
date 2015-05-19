93dde4ad004ea08465c079c796579fef8041f097
licheng <lalalic@139.com>
2015-05-19 23:23:33 +0800

    refactor to provide framework for qili apps

--- index.js
+++ index.js
@@ -1,51 +1,16 @@
-var React=require('react/addons'),
-    {Component}=React,
-    {init}=require('./lib/db/index'),
-    Application=require('./lib/db/app'),
+var {init,Model,User,Role,File,Log}=require("./lib/db"),
+    React=require('react/addons'),
+    {Component}=React;
 
-    Router= require('react-router'),
-    {Route, NotFoundRoute, Link, State, DefaultRoute} = Router,
-    Main=require("./lib/main");
 
-Main.defaultProps.title="Qili Admin"
+exports.React=React
+exports.Component=Component
 
-var Dashboard=require('./lib/dashboard'),
-    routes=(
-    <Route name="main" path="/" handler={Main}>
-        <Route name="dashboard" handler={Dashboard}/>
-        <Route name="app" handler={require('./lib/app')}/>
-        <Route name="cloud" handler={require('./lib/cloud')}/>
-        <Route name="data" handler={require('./lib/data')}/>
+exports.init=init
+exports.Model=Model
+exports.User=User
+exports.Role=Role
+exports.File=File
+exports.Log=Log
 
-        <DefaultRoute handler={Dashboard}/>
-    </Route>
-);
-
-class Messager extends Component{
-    constructor(props){
-        super(props)
-        this.state={message:null, type:null}
-    }
-
-    render(){
-        return (<div className={"messager "+(this.state.type||"")}>{this.state.message}</div>)
-    }
-}
-
-function onReady(){
-    var messager=React.render(<Messager/>, document.body)
-
-    init("http://192.168.0.105:9080/1/","admin",function(db){
-        Application.init(db).then(function(){
-            Router.run(routes, function(Handler, state){
-                React.render(<Handler
-                    params={state.params}
-                    query={state.query}/>, document.body)
-            })
-        })
-    }, function(error){
-        messager.setState({message:"Server Error:"+error,type:'error'})
-    })
-}
-
-typeof(document.ondeviceready)!='undefined' ? document.ondeviceready(onReady) : onReady();
+exports.Main=require('./lib/main')
