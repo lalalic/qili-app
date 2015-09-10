96c6371a6b3c053fe91154381f3228f95153b47b
licheng <lalalic@139.com>
2015-09-11 00:53:37 +0800

    auto deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,31 @@
 var gulp=require('gulp'),
-    shell=require('gulp-shell');
+    shell=require('gulp-shell'),
+    allin1;
 
-gulp.task('javascript',shell.task('watchify -d -g uglifyify index.js -o www/index.js -i jquery'))
-    .task('minify', shell.task('node_modules/.bin/uglifyjs www/index.js > www/index.min.js'))
-    .task('product', function(){
-        gulp.watch(['www/index.js'],['minify']).on('change', function(){
-            var fs=require('fs'), root="www/";
+gulp.task('javascript', shell.task('browserify -d index.js -o www/index.js -i jquery'))
+    .task('allin1', allin1=function(){
+        var fs=require('fs')
 
-            fs.readFile(root+"index.html", 'utf8', function(error, html){
-                if(error)
-                    return console.log(error);
-                var js=fs.readFileSync(root+'index.min.js','utf8')
+        var html=fs.readFileSync("www/index.html", 'utf8')
+        var js=require('uglifyjs').minify('www/index.js').code
 
-                html=html.replace('<script type="text/javascript" src="index.js"></script>',
-                        '<script type="text/javascript">'+js+'</script>');
-                fs.writeFile(root+"allin1.html", html, 'utf8', function(error){
-                    if(error)
-                        console.log(error)
-                })
-            })
-        })
+        var data=html.split(/<script.*\/script>/i)
+        data.splice(1,1,'<script type="text/javascript">'+js+'</script>')
+
+        fs.writeFileSync("www/allin1.html", data.join(''), 'utf8')
     })
-    .task('watchmock', function(){
-		 gulp.watch(['mock.json'],['javascript']).on('change', function(){
-			console.log("mock.json changed, rebuild javascript")
-		 })
+    .task('watch', ['javascript'], function(){
+		 gulp.watch(['mock.json','index.js','lib/**/*.*'],['javascript'])
+
+         gulp.watch(['www/index.js','www/index.html'],['allin1'])
+
+         console.log('Starting allin1...')
+         allin1()
+         console.log('Finished allin1...')
+
 	})
-    .task('mock',['watchmock'], shell.task('"node_modules/.bin/restmock"'))
-    .task('default',['mock','javascript'],function(){
-        /*
-         * can't be here since it's blocked by mock and javascript,
-         * so make seperated watch task, and make default task dependent
-         * on watch tasks
-         */
-    })
+    .task('default',['watch'], shell.task('"node_modules/.bin/restmock"'))
+
     .task('cordovaCreate',shell.task(['cordova create cordova lalalic.superdaddy superdaddy --link-to=www']))
     .task('cordovaConfig', ['cordovaCreate'], function(){
         var fs=require('fs'),
