f63ba308d84896511f83dd340422366661de9bf6
lalalic <lalalic.gmail.com>
2015-09-11 09:39:07 +0800

    auto deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,24 @@
 var gulp=require('gulp'),
-    shell=require('gulp-shell'),
-    allin1;
+    shell=require('gulp-shell');
 
 gulp.task('javascript', shell.task('browserify -d index.js -o www/index.js -i jquery'))
-    .task('allin1', allin1=function(){
+    .task('allin1',function(){
         var fs=require('fs')
 
         var html=fs.readFileSync("www/index.html", 'utf8')
-        var js=require('uglifyjs').minify('www/index.js').code
+        try{
+            var js=require('uglify-js').minify('www/index.js').code
+            var data=html.split(/<script.*\/script>/i)
+            data.splice(1,1,'<script>'+js+'</script>')
 
-        var data=html.split(/<script.*\/script>/i)
-        data.splice(1,1,'<script type="text/javascript">'+js+'</script>')
-
-        fs.writeFileSync("www/allin1.html", data.join(''), 'utf8')
+            fs.writeFileSync("www/allin1.html", data.join(''), 'utf8')
+        }catch(error){}
     })
-    .task('watch', ['javascript'], function(){
+    .task('watch', function(){
 		 gulp.watch(['mock.json','index.js','lib/**/*.*'],['javascript'])
-
          gulp.watch(['www/index.js','www/index.html'],['allin1'])
-
-         console.log('Starting allin1...')
-         allin1()
-         console.log('Finished allin1...')
-
 	})
-    .task('default',['watch'], shell.task('"node_modules/.bin/restmock"'))
+    .task('default',['javascript','watch'], shell.task('"node_modules/.bin/restmock"'))
 
     .task('cordovaCreate',shell.task(['cordova create cordova lalalic.superdaddy superdaddy --link-to=www']))
     .task('cordovaConfig', ['cordovaCreate'], function(){
