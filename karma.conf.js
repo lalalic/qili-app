5c67252ce9fc0e4395c24eaf6f19f3660212d5af
licheng <lalalic@139.com>
2016-01-10 00:11:05 +0800

    fix bugs

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'spec/**/*Spec.js'
+        'spec/**/userSpec.js',
+        'spec/**/modelSpec.js'
     ],
 
 
@@ -27,7 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'spec/**/dataSpec.js': ['browserify']
+        'spec/**/*Spec.js': ['browserify']
     },
 //****the browserify of karma-browserify has issue with debug, so remove it to use main browserify
     browserify:Object.assign({debug:true},require('./package.json').browserify),
