b7b4c034f3b29329d196702f8bbf62b9d84f28df
licheng <lalalic@139.com>
2016-01-21 06:53:24 +0800

    test appui

--- spec/ui/appSpec.js
+++ spec/ui/appSpec.js
@@ -3,22 +3,42 @@ import {initWithUser, spyOnXHR, ajaxHaveBeenCalled, failx, root} from "../db/hel
 import App from "../../lib/db/app"
 import User from "../../lib/db/user"
 import MyUI from "../../lib/app"
+import {TextField} from "material-ui"
 
 describe("App UI", function(){
     let AppUI=injectTheme(MyUI)
 
-    it("can show current existing application",()=>{
+    fit("can show current existing application",()=>{
         spyOn(App,"isRemovable").and.returnValue(true)
-        let props={app:{_id:`app${uuid()}`, name:"test"}}
-            ,ui=TestUtils.renderIntoDocument(<AppUI {...props}/>)
+        let props={app:{_id:`app${uuid()}`, name:"test", uname:"uname", apiKey:"adfasdf"}}
+            ,render=TestUtils.renderIntoDocument(<AppUI {...props}/>)
+            ,ui=TestUtils.findRenderedComponentWithType(render,MyUI)
             ,cmdRemove=findCommand(ui, "Remove")
             ,cmdUpload=findCommand(ui, "Upload")
             ,cmdSave=findCommand(ui,"Save")
+            ,textFields=TestUtils.scryRenderedComponentsWithType(ui,TextField)
 
         expect(cmdSave).toBeFalsy()
         expect(cmdRemove).toBeTruthy()
         expect(cmdUpload).toBeTruthy()
 
+        expect(textFields.length).toBe(4)
+        expect(textFields[0].getValue()).toBe(props.app.name)
+        expect(textFields[1].getValue()).toBe(props.app.uname)
+        expect(textFields[2].getValue()).toBe(props.app.apiKey)
+        expect(textFields[3].getValue()).toMatch(new RegExp(`${props.app.apiKey}/wechat`,"ig"))
+
+        spyOn(App,'upsert')
+        spyOn(ui,'setState').and.callThrough()
+        let inName=textFields[0],
+            input=TestUtils.findRenderedDOMComponentWithTag(inName,'input')
+        input.value=("test1")
+        TestUtils.Simulate.change(input)
+        expect(ui.setState).toHaveBeenCalled()
+        expect(ui.state.app.name).toBe("test1")
+        debugger
+        TestUtils.Simulate.blur(input)
+        expect(App.upsert).toHaveBeenCalled()
     })
 
     it("can add new application", ()=>{
@@ -27,10 +47,16 @@ describe("App UI", function(){
             ,cmdRemove=findCommand(ui, "Remove")
             ,cmdUpload=findCommand(ui, "Upload")
             ,cmdSave=findCommand(ui,"Save")
+            ,textFields=TestUtils.scryRenderedComponentsWithType(ui,TextField)
 
             expect(cmdSave).toBeTruthy()
             expect(cmdRemove).toBeFalsy()
             expect(cmdUpload).toBeFalsy()
+
+            expect(textFields[0].getValue()).toBe("")
+            expect(textFields[1].getValue()).toBe("")
+            expect(textFields[2].getValue()).toBe("")
+            expect(textFields[3].getValue()).toBe("")
     })
 
     it("can not remove built-in application", ()=>{
@@ -41,6 +67,7 @@ describe("App UI", function(){
             ,cmdRemove=findCommand(ui, "Remove")
             ,cmdUpload=findCommand(ui, "Upload")
             ,cmdSave=findCommand(ui,"Save")
+            ,textFields=TestUtils.scryRenderedComponentsWithType(ui,TextField)
 
             expect(cmdSave).toBeFalsy()
             expect(cmdRemove).toBeFalsy()
