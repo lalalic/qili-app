dee195f9e11679b8f9d629df66118c231770950e
licheng <lalalic@139.com>
2016-12-31 10:50:25 +0800

    clean scripts

--- .test.mock.js
+++ .test.mock.js
@@ -1,1 +1,4 @@
-require('restmock');module.exports=require('./lib/main') 
+require('restmock')
+require("./lib").QiliApp.WrappedComponent.defaultProps.service='http://localhost:9080/1/'
+require("./package.json").homepage="http://localhost:"+require("./mock.json").port
+module.exports=require('./lib/main')
