a07536e496bb01ac85932a8e6a88165e926f66e4
Li <lalalic@gmail.com>
2018-04-20 18:23:24 +0800

    debugable

--- rollup.config.js
+++ rollup.config.js
@@ -9,76 +9,74 @@ const _external=externals=>id=>{
 	return !!externals.find(a=>id==a||id.startsWith(a+'/'))
 }
 
-export default args=>{
-	return {
-		output:{
-			exports:"named",
+export default {
+	output:{
+		exports:"named",
+	},
+	external:_external(
+	  Object.keys(dependencies)
+		.concat(Object.keys(peerDependencies))
+		.filter(a=>!!a)
+	),
+	plugins: [
+		less({
+			insert:true,
+			output(code){
+				return code
+			}
+		}),
+		babel({
+			babelrc:false,
+			presets: [
+				["env", {modules:false}],
+				"react",
+			],
+			exclude: ["node_modules/**"],
+			plugins:[
+				"babel-plugin-external-helpers",
+				//@@@can't use this for rollup, otherwise it will add module.exports=exports["default"]
+				//"babel-plugin-add-module-exports",
+				"babel-plugin-transform-object-rest-spread",
+				"babel-plugin-transform-class-properties",
+				"babel-plugin-relay",
+			]
+		}),
+		{
+			name:"relay-graphql",
+			transform(code,id){
+				const REG=/require\("(\.\/__generated__\/(.*)\.graphql)"\)/gi
+				
+				let imports=[]
+				code=code.replace(REG, function(match, required, key){
+					imports.push(`import ${key} from "${required}"`)
+					return key
+				})
+				
+				if(imports.length>0){
+					code=imports.join("\n")+'\n'+code
+				}
+
+				return {
+					code,
+					map:null
+				}
+			}
 		},
-		external:_external(
-		  Object.keys(dependencies)
-			.concat(Object.keys(peerDependencies))
-			.filter(a=>!!a)
-		),
-		plugins: [
-			less({
-				insert:true,
-				output(code){
-					return code
-				}
-			}),
-			babel({
-				babelrc:false,
-				presets: [
-					["env", {modules:false}],
-					"react",
+		commonjs({
+			namedExports:{
+				'node_modules/react/index.js': [
+					'Component', 'PureComponent',
+					'Children', 'createElement',
+					'Fragment', 'createFactory'
 				],
-				exclude: ["node_modules/**"],
-				plugins:[
-					"babel-plugin-external-helpers",
-					//@@@can't use this for rollup, otherwise it will add module.exports=exports["default"]
-					//"babel-plugin-add-module-exports",
-					"babel-plugin-transform-object-rest-spread",
-					"babel-plugin-transform-class-properties",
-					"babel-plugin-relay",
-				]
-			}),
-			{
-				name:"relay-graphql",
-				transform(code,id){
-					const REG=/require\("(\.\/__generated__\/(.*)\.graphql)"\)/gi
-					
-					let imports=[]
-					code=code.replace(REG, function(match, required, key){
-						imports.push(`import ${key} from "${required}"`)
-						return key
-					})
-					
-					if(imports.length>0){
-						code=imports.join("\n")+'\n'+code
-					}
+				"node_modules/immutable/dist/immutable.js":[
+					"List","Map","Collection"
+				],
+				'node_modules/prop-types/index.js':
+					"string,object,bool,node,number,oneOfType,func".split(",")
 
-					return {
-						code,
-						map:null
-					}
-				}
-			},
-			commonjs({
-				namedExports:{
-					'node_modules/react/index.js': [
-						'Component', 'PureComponent',
-						'Children', 'createElement',
-						'Fragment', 'createFactory'
-					],
-					"node_modules/immutable/dist/immutable.js":[
-						"List","Map","Collection"
-					],
-					'node_modules/prop-types/index.js':
-						"string,object,bool,node,number,oneOfType,func".split(",")
-
-				}
-			}),
-			!args.environment ? minify() : {}
-		]
-	}
+			}
+		})
+	]
 }
+
