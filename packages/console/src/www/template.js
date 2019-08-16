1c9ae01e2b4694053e92131f860f89ac1b29ba87
lalalic <lalalic@139.com>
2019-08-16 20:10:53 +0800

    fix

--- packages/console/src/www/template.js
+++ packages/console/src/www/template.js
@@ -1,15 +1,17 @@
 export default function({content}){
-    return `<html>
+    return `<!doctype html><html>
         <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
             <title>七里云</title>
         </head>
         <body style="margin:0">
             <header style="position:fixed;top:0px;width:100%;height:50px;line-height:50px;display:flex;flex-direction:row;background-color:#303848;color:white;">
                 <center style="flex:1 1 100%"></center>
-                <center style="flex:1;padding-right:20;margin:auto">
-                    <button style="padding:10;white-space:nowrap; border-radius:5; border:1px solid white;background:transparent;color:white">
+                <center style="flex:1;padding-right:20px;margin:auto">
+                    <a href="https://app.qili2.com" style="padding:10px;white-space:nowrap; border-radius:5px; border:1px solid white;background:transparent;color:white">
                         管理控制台
-                    </button>
+                    </a>
                 </center>
             </header>
             <article style="min-height:500px;margin-top:55px" id="app">
